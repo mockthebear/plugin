e87e0617e3fc708348f0232424bbdc19e7fe5efa
Matheus Braga <matheus@gocache.com.br>
2022-05-24 12:40:22 -0300

    a

--- main.lua
+++ main.lua
@@ -4,9 +4,15 @@ local ngx = ngx
 local _M = {}
 
 function _M.header_filter()
-
+   ngx.log(ngx.ERR, "owo sussy baka")
    ngx.header["X-My-Header"] =  "2"
 
 end
+
+
+function _M.log()
+   ngx.log(ngx.ERR, "The greater UwU")
+end
+
 
 return _M
